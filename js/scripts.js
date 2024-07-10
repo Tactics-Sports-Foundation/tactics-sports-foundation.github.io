0f80bfc82bb52f31125f6b82b7bda627fb489fa5
Tactics-Sports-Foundation <tacticssportsfoundation@gmail.com>
2024-07-10 13:11:16 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,34 @@ window.onclick = function (event) {
     }
 }
 
+
+let currentlyOpenContent = null;
+let currentlyOpenIcon = null;
+
+document.querySelectorAll('.view-participants-btn').forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        var content = this.closest('.acheive-card').querySelector('.card-content');
+        var icon = this.querySelector('i');
+
+        if (currentlyOpenContent && currentlyOpenContent !== content) {
+            currentlyOpenContent.style.display = "none";
+            currentlyOpenIcon.classList.remove('fa-chevron-up');
+            currentlyOpenIcon.classList.add('fa-chevron-down');
+        }
+
+        if (content.style.display === "none" || content.style.display === "") {
+            content.style.display = "grid";
+            icon.classList.remove('fa-chevron-down');
+            icon.classList.add('fa-chevron-up');
+            currentlyOpenContent = content;
+            currentlyOpenIcon = icon;
+        } else {
+            content.style.display = "none";
+            icon.classList.remove('fa-chevron-up');
+            icon.classList.add('fa-chevron-down');
+            currentlyOpenContent = null;
+            currentlyOpenIcon = null;
+        }
+    });
+});
+
